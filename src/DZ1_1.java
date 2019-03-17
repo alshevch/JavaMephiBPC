7feca219056d0cf30ac4873d9b0cf2390760e292
alshevch <48517049+alshevch@users.noreply.github.com>
2019-03-17 21:53:52 +0300

    HW 1 is completed

---
+++ src/DZ1_1.java
@@ -0,0 +1,13 @@
+
+public class DZ1_1 {
+    public static void main (String[] args){
+        int a=3;
+        int b=1;
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println("a="+a+" and "+"b="+b);
+
+    }
+
+}
---
+++ src/DZ1_2.java
@@ -0,0 +1,11 @@
+public class DZ1_2
+        public static void main (String[] args){
+            int a=25;
+            char b;
+            double c;
+            c=a/8;
+
+
+            System.out.println("Результат:"+b);
+        }
+}
